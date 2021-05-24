0ef7bb636681e73b31a71e9def4683edec376f48
Noam <noamsi@post.bgu.ac.il>
2021-05-24 17:40:11 +0300

    Improved Q1

--- HW2.jl
+++ HW2.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ f4f05182-38b6-4bfc-bcbb-86ceb63cecbb
-using Distributions, Turing, StatsPlots, Random, RDatasets, CSV
+using Distributions, Turing, StatsPlots, Random, RDatasets, CSV, DataFrames, CategoricalArrays
 
 # ╔═╡ cff83100-b955-11eb-2950-75483cd235df
 md""" 
@@ -31,10 +31,19 @@ md"""
 """
 
 # ╔═╡ 74905b50-46b0-41a1-86bb-5d151e3e3a58
-warpbreaks_df = RDatasets.dataset("datasets", "warpbreaks")
+begin
+	warpbreaks_df = RDatasets.dataset("datasets", "warpbreaks")
+	transform!(warpbreaks_df, [:Wool, :Tension] => ((w, t) -> categorical(string.(w,t))) => "wool_tension")
+end
+
+# ╔═╡ e597b2c1-82d9-44fc-bcb8-20de1a15269d
+categorical(["A", "B"])
+
+# ╔═╡ 8b8e093c-1a42-40b1-8775-53cdf9cde072
+warpbreaks_df
 
 # ╔═╡ 8e981ff6-e0a1-45f6-98ef-113432c13808
-histogram(warpbreaks_df.Breaks, bins=20,xlabel="Count of Warp Breaks", ylabel="Count of Looms", label="All Data")
+histogram(warpbreaks_df.Breaks, bins=20,xlabel="Count of Warp Breaks", ylabel="Count of Looms", lab="all weaves", title="Weaves")
 
 # ╔═╡ b44ae535-e44e-4dbc-a97e-bb50ca90e2bb
 summarystats(warpbreaks_df.Breaks), std(warpbreaks_df.Breaks)
@@ -42,7 +51,7 @@ summarystats(warpbreaks_df.Breaks), std(warpbreaks_df.Breaks)
 # ╔═╡ fa5603e5-7d9d-453a-9c96-7d6a77f12127
 md"""
 
-#### Seperate on Wool Type
+#### Data: Wool
 
 """
 
@@ -50,7 +59,7 @@ md"""
 let
 	bins = 0:5:100
 	xlim = (0,100)
-	@df warpbreaks_df groupedhist(:Breaks, group=:Wool, bar_position = :dodge, bins=bins, ylabel="Loom Counts")
+	@df warpbreaks_df groupedhist(:Breaks, group=:Wool, bar_position = :dodge, bins=bins, xlabel="Warp Breaks", ylabel="Weave Counts", title="Warp Breaks by Wool")
 end
 
 # ╔═╡ 60951dfa-e531-4fcd-ac2f-eab1bd7ba90b
@@ -62,6 +71,23 @@ Observate that on average, **wool of type A breaks more often than wool of type 
 
 """
 
+# ╔═╡ 05541d77-be89-4a0d-ac7e-71738c7d12b1
+md"""
+
+#### Data: Tension
+
+"""
+
+# ╔═╡ 3b5c9837-4a12-4bc5-81b1-7b425a640465
+let
+	bins = 0:5:100
+	xlim = (0,100)
+	@df warpbreaks_df groupedhist(:Breaks, group=:Tension, bar_position = :dodge, bins=bins, xlabel="Warp Breaks", ylabel="Weave Counts", title="Warp Breaks by Tension")
+end
+
+# ╔═╡ 5ac5e359-6964-41e4-a7ee-fb85756f130a
+
+
 # ╔═╡ 24ebf4be-0b4e-4158-ba05-b71ddfec3c44
 md"""
 ## Model 1: Fully Pooled
@@ -69,17 +95,27 @@ md"""
 The Poisson distribution gives the probability of observing some $k \in \mathbb{N}$ events in a given period of time, assuming that events occur independently at a constant rate. According to [^1], it can be used to model the distribution of the number of defects in a piece of material. Since every break in the wool is caused independently with no memory of the previous breaks, we will use the Poisson distribution to model the number of warp breaks in a loom:
 
 $λ_0 = 27$
-$λ_1 ∼ Exponential(λ_0)$
-$obs[i] ∼ Poisson(λ_1) \space \forall i$
+$λ_A ∼ Exponential(λ_0)$
+$λ_B ∼ Exponential(λ_0)$
+$obs[i] ∼ Poisson(λ_{W_i}) \space \forall i$
 
-Where $obs[i]$ is the number of breaks in the $i$'th loom.
+Where $obs[i]$ is the number of breaks in the $i$'th loom, and $W_i$ is its wool type.
 """
 
 # ╔═╡ 9ff9b9b0-3438-438d-920f-efb32a27cbca
-@model function warp_breaks(breaks)
+@model function warp_breaks(breaks, wool)
 	λ0 = 27
-	λ1 ~ Exponential(λ0)
-	breaks ~ product_distribution(fill(Poisson(λ1), length(breaks)))
+	λA ~ Exponential(λ0)
+	λB ~ Exponential(λ0)
+	for i in eachindex(breaks)
+		if wool[i] == "A"
+			breaks[i] ~ Poisson(λA)
+		elseif wool[i] == "B"
+			breaks[i] ~ Poisson(λB)
+		else
+			throw(ErrorException("Invalid Wool Type"))
+		end
+	end
 end
 
 # ╔═╡ 6c478f37-ecb9-4a54-a252-8bfc21251a24
@@ -88,13 +124,13 @@ md"""
 """
 
 # ╔═╡ 4d6cf4f7-961a-4fb9-8ea6-5babe84cafa7
-chn1_prior = sample(warp_breaks(warpbreaks_df.Breaks), Prior(), 10000)
-
-# ╔═╡ d334e9d1-f55c-44ea-b3bc-5b7afb7df84c
-plot(chn1_prior)
+chn1_pooled_prior = sample(warp_breaks(warpbreaks_df.Breaks, warpbreaks_df.Wool), Prior(), 10000)
 
 # ╔═╡ 58675fd9-d1fb-4d09-9879-12b495fa154a
-describe(chn1_prior)
+describe(chn1_pooled_prior)
+
+# ╔═╡ d334e9d1-f55c-44ea-b3bc-5b7afb7df84c
+plot(chn1_pooled_prior)
 
 # ╔═╡ 520a5338-d39d-4a41-a133-f9257a6b312e
 md"""
@@ -102,43 +138,33 @@ md"""
 """
 
 # ╔═╡ 7b61939d-4fdd-4cf3-9396-bc669d79c69d
-chn1 = sample(warp_breaks(warpbreaks_df.Breaks), NUTS(), MCMCThreads(), 10000, 4)
+chn1_pooled = sample(warp_breaks(warpbreaks_df.Breaks, warpbreaks_df.Wool), NUTS(), MCMCThreads(), 10000, 4)
+
+# ╔═╡ 5da2340b-0ef1-4454-83ba-8557fada0b98
+describe(chn1_pooled)
 
 # ╔═╡ c1e5aa03-e300-477b-bd42-b4d0c14457b2
-plot(chn1)
+plot(chn1_pooled)
 
 # ╔═╡ c7b066a2-cc0d-4d85-b7f9-a279b3345ee4
 md"""
 
-We can see that the sampler converges (all 4 chains are in agreement).
-
-##### Seperating Wool Type
-Let us estimate $λ_1$ seperately for each wool type:
+We can see that the sampler converges (all 4 chains are in agreement). 
 """
-
-# ╔═╡ c8d43349-488d-4cd3-934b-88d334127272
-begin
-	chn1_A = sample(warp_breaks(warpbreaks_df[warpbreaks_df.Wool .== "A", :Breaks]), NUTS(), MCMCThreads(), 10000, 1)
-	chn1_B = sample(warp_breaks(warpbreaks_df[warpbreaks_df.Wool .== "B", :Breaks]), NUTS(), MCMCThreads(), 10000, 1);
-end
-
-# ╔═╡ ff0dbf9b-0135-4c68-883b-ac30283ff9c7
-begin
-	plot(chn1_A, label="Wool A")
-	plot!(chn1_B, label="Wool B", legend=:topright)
-end
 
 # ╔═╡ c5963b29-f843-4e38-ab21-51b3d891a197
 md"""
-## Plotting Prior and Posterior
-Let's now plot the prior, posterior, and mean observation of $λ_1$:
+#### Plotting Prior and Posterior
+The posterior is in agreement with the data: mean warp breaks with wool type B is less than mean warp breaks with wool of type A, and there is a strong mode at the observed mean.
+Let's now plot the prior, posterior, and mean observation of $λ_A, λ_B$:
 """
 
 # ╔═╡ 88f73f6d-0709-46fb-b7a6-a9898a2f044c
 begin
-	density(chn1[:,:,1], lab="posterior", color=:red)  # A density plot of the 1st sampled chain
-	vline!([mean(warpbreaks_df.Breaks)], linewidth = 2, color=:yellow, label="mean observation",)  # The mean observation
-	density!(chn1_prior, label="prior",  legend=:topright, color=:cyan)  # The prior
+	density(chn1_pooled[:,:,1], lab="posterior", color=:red)  # A density plot of the 1st sampled chain
+	density!(chn1_pooled_prior, label="prior",  legend=:topright, color=:cyan)  # The prior
+	vline!([mean(warpbreaks_df[warpbreaks_df.Wool .== "A",:Breaks])], linewidth = 2, subplot=1, color=:yellow, label="mean observation for wool A",)  # The mean observation for wool A
+	vline!([mean(warpbreaks_df[warpbreaks_df.Wool .== "B",:Breaks])], linewidth = 2, subplot=2, color=:yellow, label="mean observation for wool B",)  # The mean observation for wool B
 end
 
 # ╔═╡ b2f5b368-2940-442d-936f-58aec32c889e
@@ -146,67 +172,104 @@ md"""
 The fact that the prior is so flat compared to the posterior alludes to the fact that it is a rather uninformative prior.
 """
 
-# ╔═╡ d4974a79-c017-42b9-85b5-b21d46fddf0b
-md"""
-#### Seperating Wool Type
-"""
-
-# ╔═╡ 82e92527-9e2d-4427-bd84-6676b69ef9be
-begin
-	density(chn1_A, lab="posterior (wool A)", color=:pink)  # A density plot of the 1st sampled chain
-	density!(chn1_B, lab="posterior (wool B)", color=:red)  # A density plot of the 1st sampled chain
-	density!(chn1_prior, label="prior",  legend=:topright, color=:cyan)  # The prior
-end
-
-# ╔═╡ 81251a40-160a-4a5b-bd5c-ca83de92a190
-md"""
-When fed just one category of data, the inferences produce different results. The posteriors seperated on $A$ and $B$ reflect the same trend we saw before, in the data: wool of type $A$ is estimated to break more often than wool of type $B$.  
-"""
-
 # ╔═╡ 9fd59715-6aaf-4786-b417-39f795811e52
 md"""
 ## Model 2: Fully Seperate
-Here, we let each experiment have it's own set of parameters (not shared).
+Here, we account for warp tension $T$ as well. We let each parameter $W, T$ have it's own set of parameters (not shared).
+
+
+$λ_0 = 27$
+$λ_1[W] ∼ Exponential(λ_0) \space \forall W \in \{A, B\}$
+$λ_2[T] ∼ Exponential(λ_0) \space \forall T \in \{L, M, H\}$
+$obs[i] ∼ Poisson(λ_1[W_i]) + Poisson(λ_2[T_i]) \space \forall i$
+
+Where $obs[i]$ is the number of breaks in the $i$'th loom.
 
 """
 
 # ╔═╡ 70c61890-a65f-49a8-be91-9bb9ddbcbf00
-@model function warp_breaks_seperate(breaks, tension)
+@model function warp_breaks_seperate(breaks, wool, tension)
 	λ0 = 27
-	λ1 ~ product_distribution(fill(Exponential(λ0), length(breaks)))
+	λA ~ Exponential(λ0)
+	λB ~ Exponential(λ0)
+	λL ~ Exponential(λ0)
+	λM ~ Exponential(λ0)
+	λH ~ Exponential(λ0)
+	
 	for i in eachindex(breaks)
-		breaks[i] ~ Poisson(λ1[i])
+		if wool[i] == "A"
+			λ1 = λA
+		elseif wool[i] == "B"
+			λ1 = λB
+		else
+			throw(ErrorException("Invalid Wool Type"))
+		end
+
+		if tension[i] == "L"
+			λ2 = λL
+		elseif tension[i] == "M"
+			λ2 = λM
+		elseif tension[i] == "H"
+			λ2 = λH
+		else
+			throw(ErrorException("Invalid Tension Type"))
+		end
+
+		breaks[i] ~ Poisson(λ1 + λ2)
 	end
 end
 
+# ╔═╡ 6c582d07-1dcd-4896-bc92-cd419c8ef78c
+md"""
+#### Sampling the Prior
+"""
+
+# ╔═╡ fa906b36-9d5a-45f9-ba21-93276e663e62
+chn1_seperate_prior = sample(warp_breaks_seperate(warpbreaks_df.Breaks, warpbreaks_df.Wool, warpbreaks_df.Tension), Prior(), 10000)
+
+# ╔═╡ 57e2e8c3-653d-46c2-88a1-bb3fcae985eb
+describe(chn1_seperate_prior)
+
+# ╔═╡ f247548c-d548-46b9-bc48-c39c7818fbd3
+plot(chn1_seperate_prior)
+
+# ╔═╡ 0c0328b0-57db-4958-a80a-9911d40a1382
+md"""
+#### Sampling the Posterior
+"""
+
 # ╔═╡ c53f412d-d763-4def-838b-517b5c4e9d81
-chn1_seperate = sample(warp_breaks_seperate(warpbreaks_df.Breaks, warpbreaks_df.Tension), NUTS(), MCMCThreads(), 10000, 4)
+chn1_seperate = sample(warp_breaks_seperate(warpbreaks_df.Breaks, warpbreaks_df.Wool, warpbreaks_df.Tension), NUTS(), MCMCThreads(), 16000, 4)
+
+# ╔═╡ 73bf09d1-f8bd-49a5-aa2e-fc69b6ccaa44
+describe(chn1_seperate)
 
 # ╔═╡ c89b51c7-e6ff-4cf7-9ab2-0b536784b686
 plot(chn1_seperate)
 
-# ╔═╡ a63016f7-1954-4434-bc3f-bc8cb844057e
-inferred = mean(Array(group(chn1_seperate, :λ1)), dims = 1)'
-
-# ╔═╡ 64fd1f37-63f0-45ee-be3b-514b1193c7cb
+# ╔═╡ 7b11a82c-dafd-450e-9db2-b6c6a50ba114
 md"""
-We can now plot the posterior for each mean break rate, compared to the observed break rate, for the three types of wool.
+#### Plotting Prior and Posterior
+We plot the prior and posterior distributions, and overlay the mean/2 break rate. We  divide by 2 because in this new model, the process is modelled as a sum of two processes.
+
+We can see that compared to the first model, the posteriors of $λ_A, λ_B$ have higher variance (i.e. less confident).
+
+This could be because there are more parameters, so we have less certainty per each parameter.
 """
 
-# ╔═╡ d5a0f661-41e6-4e02-b3ad-dd73ce1f30fc
-let
-	p = plot(layout = (1,3), size = (1000,300))
-	for (i, t) in enumerate(unique(warpbreaks_df.Tension))
-		indices = warpbreaks_df.Tension .== t
-		scatter!(warpbreaks_df.Breaks[indices], inferred[indices],
-			xlabel="Observed Breaks", ylabel="Inferred Mean Break Rate", title="Tension $t", subplot=i)
-		plot!([0, 80], [0, 80], subplot=i, line=(1, :dash, :green), label=missing)
-	end
-	p
+# ╔═╡ e4d63042-80d5-4bf0-82e9-a961f6b56d29
+let	
+	gdf = groupby(warpbreaks_df, [:Wool, :Tension])
+	means = combine(gdf, :Breaks => mean)
+	
+	density(chn1_seperate[:,:,1], lab="posterior", color=:red)  # A density plot of the 1st sampled chain
+	density!(chn1_seperate_prior, label="prior",  legend=:topright, color=:cyan)  # The prior
+	vline!([mean(warpbreaks_df[warpbreaks_df.Wool .== "A", :Breaks])/2], linewidth = 2, subplot=1, color=:yellow, label="mean/2 observation for wool A",)  # The mean observation for wool A
+	vline!([mean(warpbreaks_df[warpbreaks_df.Wool .== "B", :Breaks])/2], linewidth = 2, subplot=2, color=:yellow, label="mean/2 observation for wool B",)  # The mean observation for wool B
+	vline!([mean(warpbreaks_df[warpbreaks_df.Tension .== "H", :Breaks])/2], linewidth = 2, subplot=3, color=:yellow, label="mean/2 observation for tension H",)  # The mean observation for tension H
+	vline!([mean(warpbreaks_df[warpbreaks_df.Tension .== "L", :Breaks])/2], linewidth = 2, subplot=4, color=:yellow, label="mean/2 observation for tension L",)  # The mean observation for tension L
+	vline!([mean(warpbreaks_df[warpbreaks_df.Tension .== "M", :Breaks])/2], linewidth = 2, subplot=5, color=:yellow, label="mean/2 observation for tension M",)  # The mean observation for tension M
 end
-
-# ╔═╡ 8cbd6987-c9a0-471e-85ad-e987e7383b12
-unique(warpbreaks_df.Tension)
 
 # ╔═╡ b31e0f83-2f64-44c6-998c-fd7be90cedd5
 histogram(warpbreaks_df.Breaks, bins=30)
@@ -395,38 +458,43 @@ md"""
 # ╟─cff83100-b955-11eb-2950-75483cd235df
 # ╟─4b56e944-8dda-4fa9-a4e9-71d0255110aa
 # ╠═74905b50-46b0-41a1-86bb-5d151e3e3a58
+# ╠═e597b2c1-82d9-44fc-bcb8-20de1a15269d
+# ╠═8b8e093c-1a42-40b1-8775-53cdf9cde072
 # ╠═8e981ff6-e0a1-45f6-98ef-113432c13808
 # ╠═b44ae535-e44e-4dbc-a97e-bb50ca90e2bb
 # ╟─fa5603e5-7d9d-453a-9c96-7d6a77f12127
 # ╠═8912c50a-3b43-4f0f-91ad-5c2a10f9d1a1
 # ╠═60951dfa-e531-4fcd-ac2f-eab1bd7ba90b
 # ╟─c5d27891-067c-46b0-90b7-bcdae15e6b8c
+# ╟─05541d77-be89-4a0d-ac7e-71738c7d12b1
+# ╠═3b5c9837-4a12-4bc5-81b1-7b425a640465
+# ╠═5ac5e359-6964-41e4-a7ee-fb85756f130a
 # ╟─24ebf4be-0b4e-4158-ba05-b71ddfec3c44
 # ╠═9ff9b9b0-3438-438d-920f-efb32a27cbca
 # ╟─6c478f37-ecb9-4a54-a252-8bfc21251a24
 # ╠═4d6cf4f7-961a-4fb9-8ea6-5babe84cafa7
-# ╠═d334e9d1-f55c-44ea-b3bc-5b7afb7df84c
 # ╠═58675fd9-d1fb-4d09-9879-12b495fa154a
+# ╠═d334e9d1-f55c-44ea-b3bc-5b7afb7df84c
 # ╟─520a5338-d39d-4a41-a133-f9257a6b312e
 # ╠═7b61939d-4fdd-4cf3-9396-bc669d79c69d
+# ╠═5da2340b-0ef1-4454-83ba-8557fada0b98
 # ╠═c1e5aa03-e300-477b-bd42-b4d0c14457b2
 # ╟─c7b066a2-cc0d-4d85-b7f9-a279b3345ee4
-# ╠═c8d43349-488d-4cd3-934b-88d334127272
-# ╠═ff0dbf9b-0135-4c68-883b-ac30283ff9c7
 # ╟─c5963b29-f843-4e38-ab21-51b3d891a197
 # ╠═88f73f6d-0709-46fb-b7a6-a9898a2f044c
 # ╟─b2f5b368-2940-442d-936f-58aec32c889e
-# ╟─d4974a79-c017-42b9-85b5-b21d46fddf0b
-# ╠═82e92527-9e2d-4427-bd84-6676b69ef9be
-# ╟─81251a40-160a-4a5b-bd5c-ca83de92a190
-# ╠═9fd59715-6aaf-4786-b417-39f795811e52
+# ╟─9fd59715-6aaf-4786-b417-39f795811e52
 # ╠═70c61890-a65f-49a8-be91-9bb9ddbcbf00
+# ╟─6c582d07-1dcd-4896-bc92-cd419c8ef78c
+# ╠═fa906b36-9d5a-45f9-ba21-93276e663e62
+# ╠═57e2e8c3-653d-46c2-88a1-bb3fcae985eb
+# ╠═f247548c-d548-46b9-bc48-c39c7818fbd3
+# ╟─0c0328b0-57db-4958-a80a-9911d40a1382
 # ╠═c53f412d-d763-4def-838b-517b5c4e9d81
+# ╠═73bf09d1-f8bd-49a5-aa2e-fc69b6ccaa44
 # ╠═c89b51c7-e6ff-4cf7-9ab2-0b536784b686
-# ╠═a63016f7-1954-4434-bc3f-bc8cb844057e
-# ╠═64fd1f37-63f0-45ee-be3b-514b1193c7cb
-# ╠═d5a0f661-41e6-4e02-b3ad-dd73ce1f30fc
-# ╠═8cbd6987-c9a0-471e-85ad-e987e7383b12
+# ╟─7b11a82c-dafd-450e-9db2-b6c6a50ba114
+# ╠═e4d63042-80d5-4bf0-82e9-a961f6b56d29
 # ╠═b31e0f83-2f64-44c6-998c-fd7be90cedd5
 # ╠═fc4fccfa-00ae-431d-b5e7-ddb7e8cde96d
 # ╠═5ff7c63f-b926-4c11-b219-dbc6948b9cd7
